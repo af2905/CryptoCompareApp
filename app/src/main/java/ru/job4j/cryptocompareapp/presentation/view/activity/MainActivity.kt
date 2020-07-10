4c3dc0224b46882a60b06261a8cb76c25ad4eafc
af2905 <af2905g@gmail.com>
2020-07-10 09:18:43 +0300

    Added data classes in project.

---
+++ app/src/main/java/ru/job4j/cryptocompareapp/presentation/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.job4j.cryptocompareapp.presentation.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.job4j.cryptocompareapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ru/job4j/cryptocompareapp/repository/database/pojo/Datum.kt
@@ -0,0 +1,10 @@
+package ru.job4j.cryptocompareapp.repository.database.pojo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Datum(
+    @SerializedName("CoinInfo")
+    @Expose
+    val coinInfo: CoinInfo
+)
